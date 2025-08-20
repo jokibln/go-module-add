91e189c5b3f0a06c6234c8e244d223cce10fccca
Johannes Kirsch <johannes.kirsch@idealo.de>
2025-08-20 11:56:42 +0200

    add documentation

--- add.go
+++ add.go
@@ -1,5 +1,8 @@
+// Package math provides some simple mathematical functions
 package math
 
+// Add adds 2 numbers and return the sum
+// That this is fun you can see here https://www.mathsisfun.com/numbers/addition.html
 func Add(a int, b int) int {
 	return a + b
 }
